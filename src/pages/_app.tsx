a3fa7082cf5cc2fd431c12d497991f1683889610
raymansell <61888240+raymansell@users.noreply.github.com>
2021-05-02 15:45:32 -0500

    TypeScript/ESLint/Prettier setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
