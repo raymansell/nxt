7c2f27a7c8056c44b87795bff43356b9933ffc39
raymansell <61888240+raymansell@users.noreply.github.com>
2021-05-02 20:55:49 -0500

    Include Loader and global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
 import styles from '../styles/Home.module.css';
+import Loader from '../components/Loader';
 
 export default function Home() {
-  return <h1 className={styles.title}>NXT</h1>;
+  return (
+    <>
+      <h1 className={styles.title}>NXT</h1>
+      <Loader show />
+    </>
+  );
 }
